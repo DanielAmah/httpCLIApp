fb94b5013f4237f9c58e3efc37aa5e6904ff396d
DanielAmah <amahdanieljack@gmail.com>
2017-05-10 10:48:10 -0700

    add comments to weather.js file

--- app/weather.js
+++ app/weather.js
@@ -4,18 +4,18 @@
 
 	if (!city) //check if a city is provided
 	{	
-		return console.error('Please provide city');
+		return console.error('Please provide city'); // return string " please provide a city " if no city is provided
 	}
 
 	const weather = require('./weather-module'); // require the weather-module.js file
 
 	const apiKey = require('./config.json').openWeatherApiKey; // require the open weather api key from the config.json file
 
-	weather(apiKey, city, (error, results)=> 
+	weather(apiKey, city, (error, results)=>  
 	{
 	  results.list.forEach((forecast)=> // run a forEach loop to return the date text and main temp from the json file requested.
 	  {
-		console.log(forecast.dt_txt, forecast.main.temp, 'C');// display the data text, main temperature in celcius  and string C as the unit.
+		console.log(forecast.dt_txt, forecast.main.temp, 'C');// display the data text, main temperature in celcius  and string 'C' as the unit.
 	  })
 	})
 
